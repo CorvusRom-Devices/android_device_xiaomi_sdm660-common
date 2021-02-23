575cba8a3a368907929a1e85684c50e3bcf94eee
OdSazib <odsazib@gmail.com>
2021-02-24 03:23:02 +0600

    sdm660-common: Add wfd dependency blobs * From LA.UM.9.6.2.r1-03300-89xx.0
    To eliminate these errors:
    --------------------------
    CANNOT LINK EXECUTABLE "/vendor/bin/wifidisplayhalservice": library "libdisplayconfig.qti.so" not found: needed by /vendor/lib/libwfddisplayconfig_proprietary.so in namespace (default)
    CANNOT LINK EXECUTABLE "/vendor/bin/wifidisplayhalservice": library "vendor.display.config@2.0.so" not found: needed by /vendor/lib/libwfddisplayconfig_proprietary.so in namespace (default)

--- sdm660.mk
+++ sdm660.mk
@@ -218,7 +218,7 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml \
     vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@1.9.vendor
 
 # DRM
 PRODUCT_PACKAGES += \

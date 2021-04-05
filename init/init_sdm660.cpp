56f4187bb8a7e0ad2ea2d1827e65590418d905da
etahamad <etahamad@icloud.com>
2021-04-05 22:52:48 +0200

    sdm660-common: Update fingerprint to Redfin - RQ2A.210405.005

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -158,7 +158,7 @@ void vendor_load_properties()
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys");
 
    std::string product = GetProperty("ro.product.vendor.device", "");
    if (product.find("whyred") != std::string::npos)

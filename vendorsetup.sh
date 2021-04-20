28051b36dd4759f4c035e2646cb7e03dba314a9f
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-04-20 20:29:47 -0300

    sdm660-common: use proton clang
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,4 @@ rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/SDM660-Project/android_hardware_qcom_display -b 4.4 hardware/qcom-caf/msm8998/display
 git clone https://github.com/SDM660-Project/android_hardware_qcom_media -b 4.4 hardware/qcom-caf/msm8998/media
 git clone https://github.com/SDM660-Project/android_hardware_qcom_audio -b 4.4 hardware/qcom-caf/msm8998/audio
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
